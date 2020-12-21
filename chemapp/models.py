a7bbbc8bf30a53785d52d26eaf921f98012417b8
Frazz3l <46572332+Frazz3l@users.noreply.github.com>
2020-12-21 21:19:23 +0000

    commented out models to add relations

--- chemapp/models.py
+++ chemapp/models.py
@@ -34,89 +34,89 @@ class Page(models.Model):
 
 
 
-class Student(moels.Models):
-    firstName = models.CharField(max_length=128)
-    lastName = models.CharField(max_length=128)
-    #not quite sure why we need this?
-    myCampusName = models.CharField(max_length=128)
-    studentID = models.IntegerField(max_length=7,unique=True)
-    anonID = models.IntegerField(max_length=7,unique=True)
-    academicPlan = models.CharField(max_length=128)
-    #should we change this to restricted choice? integer field?
-    currentYear = models.CharField(max_length=128)
-    graduationDate = models.DateTimeField(null=True, blank=True)
-    comments = models.TextField(max_length=500)
-    #I don't really think gap year is necessary as they could start uni at any age.
-    courses = ###### need course model key
+# class Student(moels.Models):
+#     firstName = models.CharField(max_length=128)
+#     lastName = models.CharField(max_length=128)
+#     #not quite sure why we need this?
+#     myCampusName = models.CharField(max_length=128)
+#     studentID = models.IntegerField(max_length=7,unique=True)
+#     anonID = models.IntegerField(max_length=7,unique=True)
+#     academicPlan = models.CharField(max_length=128)
+#     #should we change this to restricted choice? integer field?
+#     currentYear = models.CharField(max_length=128)
+#     graduationDate = models.DateTimeField(null=True, blank=True)
+#     comments = models.TextField(max_length=500)
+#     #I don't really think gap year is necessary as they could start uni at any age.
+#     courses = ###### need course model key
 
 
 
-class Course(models.Models):
-    #I dont know the format of course codes need to check, format, max length ect
-    courseCode = models.CharField(max_length=128)
-    #any paramets here max min?
-    credits = models.IntegerField()
-
-    courseName = models.CharField(max_length=200)
-    courseShortHand = models.CharField(max_length=50)
-    #could be Integer? same problem as currentYear in Student model
-    courseYear = models.CharField()
-    semester = models.CharField()
-
-    #description could possible be an uploaded txt file so we dont have to manage length.
-    description = models.TextField(max_length = 2000)
-    comments = models.TextField(max_length=500)
-    #may have to change decimal places
-    minimumPassGrade = models.DecimlField(max_digits=5,decimal_places=2)
-
-    assessments = ##### need assessments field
-
-
-
-
-class CourseGrade(models.Model):
-    grade = models.DecimalField(max_digits=5,decimal_places=2)
-
-
-
-class Assesment(models.Model):
-    weight = models.DecimlField(max_digits=5,decimal_places=2)
-    name = models.CharField(max_length=128)
-    dueDate = models.DateTimeField(null=True, blank=True)
-    assessmentComps = ######### need other model
-
-
-
-
-
-class AssessmentGrade(models.Model):
-    #basic restricted choice options
-    LATE_STATUS = (
-        ('1', '1 Band'),
-        ('2', '2 Bands'),
-    )
-
-    GOOD_CAUSE_ACTION = (
-        ('resit', 'resit exam'),
-        ('ca', 'credit awarded'),
-    )
-
-    submissionDate = models.DateTimeField(null=True, blank=True)
-    lateStatus = models.CharField(choices = LATE_STATUS, blank = True)
-    noDetriment = models.BooleanField(default = False)
-    goodCause = models.BooleanField(default = False)
-    goodCauseAction = models.CharField(choices = GOOD_CAUSE_ACTION, blank = True)
-    markedGrade = models.DecimlField(max_digits=5,decimal_places=2)
-    # Is there any need for the penalty field?
-    #penalty = models.CharField()
-    finalGrade = models.DecimlField(max_digits=5,decimal_places=2)
-    #assessmentCompGrades = ############# no clue what this is
-
-
-class AssessmentComponents(models.Model):
-    required = models.BooleanField(default = False)
-    weight = models.DecimlField(max_digits=5,decimal_places=2)
-    description = models.CharField(max_length=100)
-
-class AssessmentComponentGrade(models.Model):
-    grade = models.DecimalField(max_digits=5,decimal_places=2)
+# class Course(models.Models):
+#     #I dont know the format of course codes need to check, format, max length ect
+#     courseCode = models.CharField(max_length=128)
+#     #any paramets here max min?
+#     credits = models.IntegerField()
+#
+#     courseName = models.CharField(max_length=200)
+#     courseShortHand = models.CharField(max_length=50)
+#     #could be Integer? same problem as currentYear in Student model
+#     courseYear = models.CharField()
+#     semester = models.CharField()
+#
+#     #description could possible be an uploaded txt file so we dont have to manage length.
+#     description = models.TextField(max_length = 2000)
+#     comments = models.TextField(max_length=500)
+#     #may have to change decimal places
+#     minimumPassGrade = models.DecimlField(max_digits=5,decimal_places=2)
+#
+#     assessments = ##### need assessments field
+#
+#
+#
+#
+# class CourseGrade(models.Model):
+#     grade = models.DecimalField(max_digits=5,decimal_places=2)
+#
+#
+#
+# class Assesment(models.Model):
+#     weight = models.DecimlField(max_digits=5,decimal_places=2)
+#     name = models.CharField(max_length=128)
+#     dueDate = models.DateTimeField(null=True, blank=True)
+#     assessmentComps = ######### need other model
+#
+#
+#
+#
+#
+# class AssessmentGrade(models.Model):
+#     #basic restricted choice options
+#     LATE_STATUS = (
+#         ('1', '1 Band'),
+#         ('2', '2 Bands'),
+#     )
+#
+#     GOOD_CAUSE_ACTION = (
+#         ('resit', 'resit exam'),
+#         ('ca', 'credit awarded'),
+#     )
+#
+#     submissionDate = models.DateTimeField(null=True, blank=True)
+#     lateStatus = models.CharField(choices = LATE_STATUS, blank = True)
+#     noDetriment = models.BooleanField(default = False)
+#     goodCause = models.BooleanField(default = False)
+#     goodCauseAction = models.CharField(choices = GOOD_CAUSE_ACTION, blank = True)
+#     markedGrade = models.DecimlField(max_digits=5,decimal_places=2)
+#     # Is there any need for the penalty field?
+#     #penalty = models.CharField()
+#     finalGrade = models.DecimlField(max_digits=5,decimal_places=2)
+#     #assessmentCompGrades = ############# no clue what this is
+#
+#
+# class AssessmentComponents(models.Model):
+#     required = models.BooleanField(default = False)
+#     weight = models.DecimlField(max_digits=5,decimal_places=2)
+#     description = models.CharField(max_length=100)
+#
+# class AssessmentComponentGrade(models.Model):
+#     grade = models.DecimalField(max_digits=5,decimal_places=2)
